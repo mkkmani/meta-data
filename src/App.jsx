a0bcc6b0b5f813d66a9f7606e3e260798b39aa09
mani <mani@email.com>
2024-04-23 15:16:44 +0530

    ....

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function WebsiteContentChecker() {
+  const [htmlCode, setHtmlCode] = useState("");
+  const [metaTitle, setMetaTitle] = useState("");
+  const [metaDescription, setMetaDescription] = useState("");
+  const [h1Tag, setH1Tag] = useState("");
+  const [pTags, setPTags] = useState([]);
+
+  const checkHtmlContent = () => {
+    const tempDiv = document.createElement("div");
+    tempDiv.innerHTML = htmlCode;
+    const metaTitle = tempDiv.querySelector("title").innerText.trim();
+    const metaDescription = tempDiv
+      .querySelector('meta[name="description"]')
+      .content.trim();
+    const h1Tag = tempDiv.querySelector("h1").innerText.trim();
+    const pTags = Array.from(tempDiv.querySelectorAll("p")).map((p) =>
+      p.innerText.trim()
+    );
+
+    setMetaTitle(metaTitle);
+    setMetaDescription(metaDescription);
+    setH1Tag(h1Tag);
+    setPTags(pTags);
+  };
+
+  return (
+    <div className="parent">
+    <div className="container">
+      <textarea
+        className="textarea"
+        rows="10"
+        cols="50"
+        placeholder="Paste HTML document code here"
+        value={htmlCode}
+        onChange={(e) => setHtmlCode(e.target.value)}
+      ></textarea>
+      <br />
+      <button className="button" onClick={checkHtmlContent}>
+        Check Content
+      </button>
+      <div className="results-container">
+        <h2>Results:</h2>
+        <div>
+          <strong>Meta Title:</strong> {metaTitle}
+          <br />
+          <strong>Meta Description:</strong> {metaDescription}
+          <br />
+          <strong>H1 Tag:</strong> {h1Tag}
+          <br />
+          <strong>Paragraph Tags:</strong>
+          <ul>
+            {pTags.map((pTag, index) => (
+              <li key={index}>{pTag}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default WebsiteContentChecker;
